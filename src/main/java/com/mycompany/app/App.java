27be2a28bb10a7ac5a7b8c2fcf032ccdd3674807
levibyte <kartol@gmx.com>
2019-02-20 03:40:42 +0400

    wro

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,45 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class App {
+
+
+    public static void main(String[] args) {
+    	//System.setProperty("webdriver.firefox.marionette","sudo /home/levibyte/maven/my-app/geckodriver");
+    	System.setProperty("webdriver.gecko.driver", "/home/levibyte/maven/my-app/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+		
+        //System.setProperty("webdriver.chrome.driver","/home/levibyte/gradle/selen/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+       
+    }
+
+}
